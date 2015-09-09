e2c8b7a220ff3d7bf070fc06f94688bcaba3ae48
Gladys Maniago <gfmaniago@gmail.com>
2015-09-08 21:31:57 -0500

    complete add  through tax

---
+++ scripts/functions.js
@@ -0,0 +1,21 @@
+//add
+
+function add(a, b) {
+	return parseFloat(a) + parseFloat(b);
+	}
+
+//substract
+
+function subtract (a, b) {
+	return parseFloat(a) - parseFloat(b);
+	}
+
+//tax
+
+function tax (a, b) {
+	return parseFloat(a) * parseFloat(b/100);
+}
+
+//StringLenght
+
+function stringLength 
