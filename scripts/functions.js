02d0aefc20abd19955510acddd3fa33da0102ba8
Gladys Maniago <gfmaniago@gmail.com>
2015-09-08 22:51:28 -0500

    complete up to nSum

--- scripts/functions.js
+++ scripts/functions.js
@@ -12,10 +12,76 @@ function subtract (a, b) {
 
 //tax
 
-function tax (a, b) {
-	return parseFloat(a) * parseFloat(b/100);
+function tax (subtotal, tax) {
+	return parseFloat(subtotal) * parseFloat(tax/100);
 }
 
 //StringLenght
 
-function stringLength 
+function stringLength (string) {
+		if (string.length<5) {
+			return 'short string';
+		} else if (string.length >=5 && string.length<=10) {
+			return 'medium string';
+		} else if (string.length >10) {
+			return 'long string';
+		}
+}
+
+//startsWith
+
+function startsWith (string) {
+	if (string [0] .toLowerCase() ==='a') {
+		return 'starts with A'
+	}
+	if (string [0] .toLowerCase() ==='b') {
+		return 'starts with B'
+	}
+	if (string [0] .toLowerCase() ==='c') {
+		return 'starts with C'
+	}
+	if (string [0] .toLowerCase() ==='d') {
+		return 'starts with D'
+	}
+	if (string [0] .toLowerCase() ==='e') {
+		return 'starts with E'
+	}
+	else {
+		return 'starts with something else'
+	}
+}
+
+//stringRepeat
+
+function stringRepeat(s, n) {
+	n = parseFloat(n);
+	return Array((n)+1).join(s);
+}
+
+//nSum
+
+function nSum (n) {
+	var total = 0;
+	n = parseFloat(n);
+	for (var i = 1; i < n+1; i++) {
+		total = total + i;
+	}
+	return total;
+}
+
+function join 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
